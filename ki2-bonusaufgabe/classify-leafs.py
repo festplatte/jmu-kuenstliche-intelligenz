f1ed48818f46ba9fec95433f59012677d00b82c6
mg <mich.gabler@gmx.de>
2019-01-02 17:21:35 +0100

    Update ki2 exercise

--- ki2-bonusaufgabe/classify-leafs.py
+++ ki2-bonusaufgabe/classify-leafs.py
@@ -1,8 +1,29 @@
-from os import listdir
+import os
+from PIL import Image
+
+traindatapath = './Train/'
+
 
 def readTrainingData():
     train_labels = []
     train_images = []
 
+    for category in os.listdir(traindatapath):
+        categorypath = traindatapath + category + '/'
+        for img in os.listdir(categorypath):
+            im = Image.open(categorypath + img)
+            train_images.append(list(im.getdata()))
+            train_labels.append(category)
+            # with open(categorypath + img, 'rb') as imgfile:
+            #     imgdata = imgfile.read()
+            #     train_images.append(imgdata)
+            #     train_labels.append(category)
+            break
+        break
 
-print(listdir('.'))
+    return [train_labels, train_images]
+
+
+result = readTrainingData()
+
+print(result)
