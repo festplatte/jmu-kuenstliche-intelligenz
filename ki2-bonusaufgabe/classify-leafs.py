ded49d74ad985cc313396093ef091e361768483e
mg <mich.gabler@gmx.de>
2019-01-11 17:23:57 +0100

    Update bonusaufgabe: working but accuracy low

--- ki2-bonusaufgabe/classify-leafs.py
+++ ki2-bonusaufgabe/classify-leafs.py
@@ -1,5 +1,8 @@
 import os
-from PIL import Image
+# from PIL import Image
+import numpy as np
+from skimage import io, color
+from skimage.transform import resize
 
 # TensorFlow and tf.keras
 import tensorflow as tf
@@ -9,17 +12,6 @@ from tensorflow import keras
 traindatapath = './Train/'
 testdatapath = './Test/'
 
-# TODO: not working yet
-def greyscaleimage(image):
-    result = []
-
-    for row in image:
-        newrow = []
-        for pixel in row:
-            newpixel = pixel[0] + pixel[1] + pixel[2]
-            newrow.append(newpixel / 765)
-        result.append(newrow)
-    return result
 
 def readimagedata(path):
     train_labels = []
@@ -28,23 +20,45 @@ def readimagedata(path):
     for category in os.listdir(path):
         categorypath = path + category + '/'
         for img in os.listdir(categorypath):
-            im = Image.open(categorypath + img)
-            train_images.append(greyscaleimage(list(im.getdata())))
+            im = resize(color.rgb2gray(io.imread(categorypath + img)), (50, 50))
+            # im = Image.open(categorypath + img).resize((50, 50), Image.ANTIALIAS).convert('L')
+            # train_images.append(list(im.getdata()))
+            train_images.append(im)
             train_labels.append(category)
+        #     break
+        # break
 
-    return [train_labels, train_images]
+    # return [train_labels, train_images]
+    return [np.array(train_labels), np.array(train_images)]
 
+# fashion_mnist = keras.datasets.fashion_mnist
+
+# (train_images, train_labels), (test_images,
+#                                test_labels) = fashion_mnist.load_data()
 
 (train_labels, train_images) = readimagedata(traindatapath)
 (test_labels, test_images) = readimagedata(testdatapath)
 
-print('Train labels: ', train_labels)
+# TODO shuffle images
+
+# train_images = train_images / 255
+# test_images = test_images / 255
+
+# print(type(train_images))
+# print(train_images)
+# print(type(train_images[0]))
+# print(train_images[0])
+# print(type(train_images[0][0]))
+# print(train_images[0][0])
+
+
+#print('Train labels: ', train_labels)
 print('Images:', len(train_images))
 
 model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Flatten(input_shape=(50, 50)),
     keras.layers.Dense(128, activation=tf.nn.relu),
-    keras.layers.Dense(10, activation=tf.nn.softmax)
+    keras.layers.Dense(33, activation=tf.nn.softmax)
 ])
 
 model.compile(optimizer=tf.train.AdamOptimizer(),
