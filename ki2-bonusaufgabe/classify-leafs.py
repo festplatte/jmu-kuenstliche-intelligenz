1d1d33dd9c1c1b4a4d594fbb2147bddca3bfa6f6
mg <mich.gabler@gmx.de>
2019-01-13 22:50:47 +0100

    Update bonusaufgabe accuracy 0.25

--- ki2-bonusaufgabe/classify-leafs.py
+++ ki2-bonusaufgabe/classify-leafs.py
@@ -7,17 +7,14 @@ from skimage.transform import resize
 # TensorFlow and tf.keras
 import tensorflow as tf
 from tensorflow import keras
-# import tflearn
-# from tflearn.layers.conv import conv_2d, max_pool_2d
-# from tflearn.layers.core import input_data, dropout, fully_connected
-# from tflearn.layers.estimator import regression
 
-
-TRAIN_DATA_PATH = './Train/'
-TEST_DATA_PATH = './Test/'
+TRAIN_DATA_PATH = './Flavia/Train/'
+TEST_DATA_PATH = './Flavia/Test/'
 IMG_SIZE = 100
 NUM_CLASSES = 33
+EPOCHS = 100
 
+random.seed(100)
 
 def readimagedata(path):
     train_labels = []
@@ -26,16 +23,11 @@ def readimagedata(path):
     for category in os.listdir(path):
         categorypath = path + category + '/'
         for img in os.listdir(categorypath):
-            # im = resize(io.imread(categorypath + img), (IMG_SIZE, IMG_SIZE))
-            im = resize(color.rgb2gray(io.imread(categorypath + img)), (IMG_SIZE, IMG_SIZE))
-            # im = Image.open(categorypath + img).resize((50, 50), Image.ANTIALIAS).convert('L')
-            # train_images.append(list(im.getdata()))
+            im = resize(io.imread(categorypath + img), (IMG_SIZE, IMG_SIZE))
+            # im = resize(color.rgb2gray(io.imread(categorypath + img)), (IMG_SIZE, IMG_SIZE))
             train_images.append(im)
             train_labels.append(category)
-        #     break
-        # break
 
-    # return [train_labels, train_images]
     return [np.array(train_labels), np.array(train_images)]
 
 def shuffledata(labels, images):
@@ -48,63 +40,27 @@ def shuffledata(labels, images):
         images[i] = images[j]
         images[j] = temp
 
+# Reading and pre-format the data
 (train_labels, train_images) = readimagedata(TRAIN_DATA_PATH)
 (test_labels, test_images) = readimagedata(TEST_DATA_PATH)
 
 shuffledata(train_labels, train_images)
 shuffledata(test_labels, test_images)
 
-# train_images = train_images / 255
-# test_images = test_images / 255
-train_images = train_images.reshape(train_images.shape[0], IMG_SIZE, IMG_SIZE, 1)
-test_images = test_images.reshape(test_images.shape[0], IMG_SIZE, IMG_SIZE, 1)
+train_images = train_images.reshape(train_images.shape[0], IMG_SIZE, IMG_SIZE, 3)
+test_images = test_images.reshape(test_images.shape[0], IMG_SIZE, IMG_SIZE, 3)
 
-# print(type(train_images))
-# print(train_images)
-# print(type(train_images[0]))
-# print(train_images[0])
-# print(type(train_images[0][0]))
-# print(train_images[0][0])
-# print(type(train_images[0][0][0]))
-# print(train_images[0][50][50])
-
-# print('Train labels: ', train_labels)
-# print('Images:', len(train_images))
-
-
-# tf.reset_default_graph()
-# convnet = input_data(shape=[None, IMG_SIZE, IMG_SIZE, 1], name='input')
-# convnet = conv_2d(convnet, 32, 5, activation='relu')
-# convnet = max_pool_2d(convnet, 5)
-# convnet = conv_2d(convnet, 64, 5, activation='relu')
-# convnet = max_pool_2d(convnet, 5)
-# convnet = conv_2d(convnet, 128, 5, activation='relu')
-# convnet = max_pool_2d(convnet, 5)
-# convnet = conv_2d(convnet, 64, 5, activation='relu')
-# convnet = max_pool_2d(convnet, 5)
-# convnet = conv_2d(convnet, 32, 5, activation='relu')
-# convnet = max_pool_2d(convnet, 5)
-# convnet = fully_connected(convnet, 1024, activation='relu')
-# convnet = dropout(convnet, 0.8)
-# convnet = fully_connected(convnet, 64, activation='softmax')
-# convnet = regression(convnet, optimizer='adam', loss='categorical_crossentropy')
-# model = tflearn.DNN(convnet, tensorboard_dir='log', tensorboard_verbose=0)
-# model.fit(train_images, train_labels, n_epoch=10, validation_set=(test_images, test_labels), snapshot_step=500, show_metric=True, run_id='my_model')
-
+# Builds the model
 model = keras.Sequential([
-    keras.layers.Conv2D(32, kernel_size=(5, 5), strides=(1, 1), activation=tf.nn.relu, input_shape=(IMG_SIZE, IMG_SIZE, 1)),
+    keras.layers.Conv2D(32, kernel_size=(5, 5), strides=(1, 1), activation=tf.nn.relu, input_shape=(IMG_SIZE, IMG_SIZE, 3)),
     keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
     keras.layers.Conv2D(64, (5, 5), activation=tf.nn.relu),
     keras.layers.MaxPooling2D(pool_size=(2, 2)),
-    # keras.layers.Conv2D(128, 5, strides=(5, 5), activation=tf.nn.relu),
-    # keras.layers.MaxPooling2D(pool_size=(5,5)),
-    # keras.layers.Conv2D(64, 5, strides=(5, 5), activation=tf.nn.relu),
-    # keras.layers.MaxPooling2D(pool_size=(5,5)),
-    # keras.layers.Conv2D(32, 5, strides=(5, 5), activation=tf.nn.relu),
-    # keras.layers.MaxPooling2D(pool_size=(5,5)),
+    keras.layers.Conv2D(128, (5, 5), activation=tf.nn.relu),
+    keras.layers.MaxPooling2D(pool_size=(2, 2)),
     keras.layers.Flatten(),
     keras.layers.Dense(1024, activation=tf.nn.relu),
-    # keras.layers.Dropout(0.8),
+    keras.layers.Dropout(0.8),
     keras.layers.Dense(NUM_CLASSES, activation=tf.nn.softmax)
 ])
 
@@ -112,9 +68,9 @@ model.compile(optimizer=tf.train.AdamOptimizer(),
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(train_images, train_labels, epochs=10)
+# Trains the model
+model.fit(train_images, train_labels, epochs=EPOCHS, validation_data=(test_images, test_labels))
+
+# Checks the accuracy
 test_loss, test_acc = model.evaluate(test_images, test_labels)
 print('Test accuracy:', test_acc)
-
-# print('Should be label: ', test_images[0])
-# print(model.predict([test_images[0]]))
