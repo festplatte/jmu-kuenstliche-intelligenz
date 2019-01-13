4ca51247b2a6bd3cd35fc9de6be0a8b54dbb7e64
mg <mich.gabler@gmx.de>
2019-01-13 16:52:33 +0100

    Update bonusaufgabe, add shuffle

--- ki2-bonusaufgabe/classify-leafs.py
+++ ki2-bonusaufgabe/classify-leafs.py
@@ -1,4 +1,5 @@
 import os
+import random
 # from PIL import Image
 import numpy as np
 from skimage import io, color
@@ -20,7 +21,7 @@ def readimagedata(path):
     for category in os.listdir(path):
         categorypath = path + category + '/'
         for img in os.listdir(categorypath):
-            im = resize(color.rgb2gray(io.imread(categorypath + img)), (50, 50))
+            im = resize(color.rgb2gray(io.imread(categorypath + img)), (100, 100))
             # im = Image.open(categorypath + img).resize((50, 50), Image.ANTIALIAS).convert('L')
             # train_images.append(list(im.getdata()))
             train_images.append(im)
@@ -31,18 +32,24 @@ def readimagedata(path):
     # return [train_labels, train_images]
     return [np.array(train_labels), np.array(train_images)]
 
-# fashion_mnist = keras.datasets.fashion_mnist
-
-# (train_images, train_labels), (test_images,
-#                                test_labels) = fashion_mnist.load_data()
+def shuffledata(labels, images):
+    for i in range(0, len(labels)):
+        j = random.randint(0, len(labels)-1)
+        temp = labels[i]
+        labels[i] = labels[j]
+        labels[j] = temp
+        temp = images[i]
+        images[i] = images[j]
+        images[j] = temp
 
 (train_labels, train_images) = readimagedata(traindatapath)
 (test_labels, test_images) = readimagedata(testdatapath)
 
-# TODO shuffle images
+shuffledata(train_labels, train_images)
+shuffledata(test_labels, test_images)
 
-# train_images = train_images / 255
-# test_images = test_images / 255
+train_images = train_images / 255
+test_images = test_images / 255
 
 # print(type(train_images))
 # print(train_images)
@@ -51,13 +58,14 @@ def readimagedata(path):
 # print(type(train_images[0][0]))
 # print(train_images[0][0])
 
+# print('Train labels: ', train_labels)
+# print('Images:', len(train_images))
 
-#print('Train labels: ', train_labels)
-print('Images:', len(train_images))
 
 model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(50, 50)),
-    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Flatten(input_shape=(100, 100)),
+    keras.layers.Dense(1024, activation=tf.nn.relu),
+    keras.layers.Dense(1024, activation=tf.nn.relu),
     keras.layers.Dense(33, activation=tf.nn.softmax)
 ])
 
@@ -65,6 +73,6 @@ model.compile(optimizer=tf.train.AdamOptimizer(),
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(train_images, train_labels, epochs=5)
+model.fit(train_images, train_labels, epochs=100)
 test_loss, test_acc = model.evaluate(test_images, test_labels)
 print('Test accuracy:', test_acc)
