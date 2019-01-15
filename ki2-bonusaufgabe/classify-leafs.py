4d96d24407e1edcf953c9efad526c5526c1d85e0
mg <mich.gabler@gmx.de>
2019-01-15 18:06:50 +0100

    Update bonusaufgabe, should increase layers use more pooling

--- ki2-bonusaufgabe/classify-leafs.py
+++ ki2-bonusaufgabe/classify-leafs.py
@@ -10,10 +10,12 @@ from tensorflow import keras
 
 TRAIN_DATA_PATH = './Flavia/Train/'
 TEST_DATA_PATH = './Flavia/Test/'
-IMG_SIZE = 100
-NUM_CLASSES = 33
-EPOCHS = 100
+IMG_SIZE = 50
+NUM_CLASSES = 32
+EPOCHS = 5
+USE_GRAYSCALE = True
 
+image_channels = 1 if USE_GRAYSCALE else 3
 random.seed(100)
 
 def readimagedata(path):
@@ -23,10 +25,12 @@ def readimagedata(path):
     for category in os.listdir(path):
         categorypath = path + category + '/'
         for img in os.listdir(categorypath):
-            im = resize(io.imread(categorypath + img), (IMG_SIZE, IMG_SIZE))
-            # im = resize(color.rgb2gray(io.imread(categorypath + img)), (IMG_SIZE, IMG_SIZE))
+            im = io.imread(categorypath + img)
+            if USE_GRAYSCALE:
+                im = color.rgb2gray(im)
+            im = resize(im, (IMG_SIZE, IMG_SIZE))
             train_images.append(im)
-            train_labels.append(category)
+            train_labels.append(int(category) - 1)
 
     return [np.array(train_labels), np.array(train_images)]
 
@@ -47,20 +51,20 @@ def shuffledata(labels, images):
 shuffledata(train_labels, train_images)
 shuffledata(test_labels, test_images)
 
-train_images = train_images.reshape(train_images.shape[0], IMG_SIZE, IMG_SIZE, 3)
-test_images = test_images.reshape(test_images.shape[0], IMG_SIZE, IMG_SIZE, 3)
+train_images = train_images.reshape(train_images.shape[0], IMG_SIZE, IMG_SIZE, image_channels)
+test_images = test_images.reshape(test_images.shape[0], IMG_SIZE, IMG_SIZE, image_channels)
 
 # Builds the model
 model = keras.Sequential([
-    keras.layers.Conv2D(32, kernel_size=(5, 5), strides=(1, 1), activation=tf.nn.relu, input_shape=(IMG_SIZE, IMG_SIZE, 3)),
-    keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+    keras.layers.Conv2D(32, kernel_size=(5, 5), strides=(1, 1), activation=tf.nn.relu, input_shape=(IMG_SIZE, IMG_SIZE, image_channels)),
+    # keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
     keras.layers.Conv2D(64, (5, 5), activation=tf.nn.relu),
-    keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2,2)),
     keras.layers.Conv2D(128, (5, 5), activation=tf.nn.relu),
     keras.layers.MaxPooling2D(pool_size=(2, 2)),
     keras.layers.Flatten(),
     keras.layers.Dense(1024, activation=tf.nn.relu),
-    keras.layers.Dropout(0.8),
+    keras.layers.Dropout(0.2),
     keras.layers.Dense(NUM_CLASSES, activation=tf.nn.softmax)
 ])
 
